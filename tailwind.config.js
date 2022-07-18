982fec6928a329cbbbacb133b93935f971a01a58
monis8khan@gmail.com <monis8khan@gmail.com>
2022-07-18 16:01:17 +0500

    added hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 module.exports = {
     content: ["./*.{html,js}"],
     theme: {
-      extend: {
-        textColor:{
-            skin:{
-                base: 'var(--color-text-base)',
-                muted: 'var(--color-text-muted)',      
-                inverted: 'var(--color-text-inverted)',     
-            }
-        },
-        backgroundColor:{
-            skin:{
-                fill: 'var(--color-fill)',
-                "button-accent": 'var(--color-button-accent)',
-                "button-accent-hover": 'var(--color-button-accent-hover)',
-                "button-accent-muted": 'var(--color-button-accent-muted)',
+        extend: {
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+            textColor: {
+                skin: {
+                    base: 'var(--color-text-base)',
+                    muted: 'var(--color-text-muted)',
+                    inverted: 'var(--color-text-inverted)',
+                }
+            },
+            backgroundColor: {
+                skin: {
+                    fill: 'var(--color-fill)',
+                    "button-accent": 'var(--color-button-accent)',
+                    "button-accent-hover": 'var(--color-button-accent-hover)',
+                    "button-accent-muted": 'var(--color-button-accent-muted)',
+                }
             }
         }
-      }
     }
-  }
+}
